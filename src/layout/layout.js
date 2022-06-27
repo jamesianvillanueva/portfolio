2dab6696a3607b02858d43c346c6db7dc22dd727
James Villanueva <jamesvillanueva659@gmail.com>
2022-06-27 17:09:11 +0800

    my-portfolio

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,12 +1,16 @@
 import Footer from "../components/Footer/footer"
 import Header from "../components/Header/header"
 import { Container } from "./style"
-
+import Head from 'next/head'
 
 
 const Layout = ({children}) => {
     return(
         <Container>
+            <Head>
+                <title>James Ian | Software Engineer</title>               
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <Header />
             <main>{ children }</main>
             <Footer />
